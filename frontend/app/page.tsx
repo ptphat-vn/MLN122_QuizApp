ebb61e64d1de2b48f5a712b09c4dc6b2a4cd67f2
ptphat-vn <tanphatphan091@gmail.com>
2026-04-21 21:54:16 +0700

    hot-fix: fix navigate

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,10 +1,5 @@
-'use client';
-
 import { redirect } from 'next/navigation';
-import { useEffect } from 'react';
 
 export default function HomePage() {
-  useEffect(() => {
-    redirect('/noi-dung');
-  }, []);
+  redirect('/noi-dung');
 }
